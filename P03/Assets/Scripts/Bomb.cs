6fa4bbad88d0a7b2e6cb0e54002c3a214771c4bd
Jory <jbb140030@utdallas.edu>
2021-04-26 11:03:05 -0500

    Bomb
    Created bomb and script to explode after timer, add forces to nearby objects

---
+++ P03/Assets/Scripts/Bomb.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+
+    public float delay = 3f;
+    public float radius = 5f;
+    public float force = 700f;
+
+    public GameObject explosionEffect;
+
+    float countdown;
+    bool hasExploded = false;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    void Explode()
+    {
+        Debug.Log("BOOM");
+
+        // Show effect
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        // Get nearby objects
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            // Add force
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if(rb != null)
+            {
+                rb.AddExplosionForce(force, transform.position, radius);
+            }
+            // Damage
+        }
+        
+        // Remove grenamde
+        Destroy(gameObject);
+    }
+}
---
+++ P03/Assets/Scripts/BombThrower.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombThrower : MonoBehaviour
+{
+    public float throwForce = 10f;
+    public GameObject bombPrefab;
+    [SerializeField] Transform bombSpawn;
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetMouseButton(0))
+        {
+            ThrowBomb();
+        }*/
+
+        if (Input.GetKeyDown("space"))
+        {
+            ThrowBomb();
+        }
+    }
+
+    void ThrowBomb()
+    {
+        GameObject bomb = Instantiate(bombPrefab, bombSpawn.position, bombSpawn.rotation);
+        Rigidbody rb = bomb.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * throwForce, ForceMode.VelocityChange);
+    }
+}
